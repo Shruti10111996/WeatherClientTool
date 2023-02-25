568cc56256053ce14aa8d67f8719a9a220270dce
sshru <sshru@DESKTOP-86NI6JC>
2023-02-25 22:33:48 +0530

    small changes

--- CityDataHelper.cs
+++ CityDataHelper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,30 +13,30 @@ namespace WeatherClientTool
     internal class CityDataHelper : ICityDataHelper
     {
         private readonly ILogger _logger;
-        
-        
+
+
         public CityDataHelper(ILogger<CityDataHelper> logger)
         {
             _logger = logger;
         }
 
-        
+
         public async Task<string> HttpCall(string url)
         {
             try
             {
                 _logger.LogInformation("HttpCall {applicationEvent} at {dateTime}", "Started", DateTime.UtcNow);
-                
+
                 using var client = new HttpClient();
 
-                var response = client.GetAsync(url);             
+                var response = client.GetAsync(url);
 
 
                 _logger.LogInformation("HttpCall {applicationEvent} at {dateTime}", "End", DateTime.UtcNow);
 
                 if (response.Result.IsSuccessStatusCode)
                 {
-                    var result= await response.Result.Content.ReadAsStringAsync();
+                    var result = await response.Result.Content.ReadAsStringAsync();
                     return result.ToString();
                 }
                 else
@@ -55,10 +56,19 @@ namespace WeatherClientTool
         {
             try
             {
-                string text = File.ReadAllText(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName+@"/data/citydata.json");
-                text=text.Replace("ā", "a");
-                var cities = JsonSerializer.Deserialize<List<CityData>>(text);
-                return cities;
+                string filePath = $"{Directory.GetCurrentDirectory()}/Data/CityData.json";
+                if (File.Exists(filePath))
+                {
+                    string text = File.ReadAllText(filePath);
+                    text = text.Replace("ā", "a");
+                    var cities = JsonSerializer.Deserialize<List<CityData>>(text);
+                    return cities;
+                }
+                else
+                {
+                    throw new Exception("File not found at :" + filePath);
+                }
+
             }
             catch (Exception ex)
             {
